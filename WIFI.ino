76c20718342802731a847379f128fba43ba7d1b2
lufetoace <38989645+lufetoace@users.noreply.github.com>
2018-05-15 15:17:47 -0500

    Add files via upload

---
+++ WIFI.ino
@@ -0,0 +1,41 @@
+
+#include "ThingSpeak.h"
+
+//#define USE_WIFI101_SHIELD
+//#define USE_ETHERNET_SHIELD
+
+#if !defined(USE_WIFI101_SHIELD) && !defined(USE_ETHERNET_SHIELD) && !defined(ARDUINO_SAMD_MKR1000) && !defined(ARDUINO_AVR_YUN) && !defined(ARDUINO_ARCH_ESP8266) && !defined(ARDUINO_ARCH_ESP32)
+#error "Uncomment the #define for either USE_WIFI101_SHIELD or USE_ETHERNET_SHIELD"
+#endif
+
+#if defined(ARDUINO_AVR_YUN)
+#include "YunClient.h"
+YunClient client;
+#else
+#if defined(USE_WIFI101_SHIELD) || defined(ARDUINO_SAMD_MKR1000) || defined(ARDUINO_ARCH_ESP8266) || defined(ARDUINO_ARCH_ESP32)
+// Use WiFi
+#ifdef ARDUINO_ARCH_ESP8266
+#include <ESP8266WiFi.h>
+#elif defined(ARDUINO_ARCH_ESP32)
+#include <WiFi.h>
+#else
+#include <SPI.h>
+#include <WiFi101.h>
+#endif
+
+char ssid[] = "NOMBRE_RED";    //  your network SSID (name)
+char pass[] = "CONTRAEÑA _RED";   // your network password
+
+int status = WL_IDLE_STATUS;
+WiFiClient  client;
+#elif defined(USE_ETHERNET_SHIELD)
+// Use wired ethernet shield
+#include <SPI.h>
+#include <Ethernet.h>
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+EthernetClient client;
+#endif
+#endif
+
+}
+
